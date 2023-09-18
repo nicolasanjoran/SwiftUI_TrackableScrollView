dbb6b33de76013cd132b9fba6118319f4d9f40ce
Nicolas Anjoran <nicolas.anjoran@gmail.com>
2023-09-18 11:58:34 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # SwiftUI_ScrollViewWithOffset
 
-A description of this package.
+A SwiftUI ScrollView with a binding for the scroll offset value.
+
+> [!IMPORTANT]
+> This has only be tested on iOS.
+
+## Example:
+
+```swift
+struct ContentView: View {
+    @State var offset = CGPoint() // Will be updated while scrolling
+    
+    var body: some View {
+        VStack {
+            Text("Offset: \(Int(offset.y))")
+            ScrollViewWithOffset(offset: $offset) {
+                LazyVStack {
+                    ForEach(Array(1...200).map { "Item \($0)" }, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+            }
+        }
+    }
+}
+```
--- Sources/SwiftUI_ScrollViewWithOffset/SwiftUI_ScrollViewWithOffset.swift
+++ Sources/SwiftUI_ScrollViewWithOffset/SwiftUI_ScrollViewWithOffset.swift
@@ -1,6 +1,69 @@
-public struct SwiftUI_ScrollViewWithOffset {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+struct ScrollOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGPoint = .zero
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
+}
+
+enum ScrollOffsetNamespace {
+    static let namespace = "ScrollViewWithTrackableOffsetNamespace"
+}
+
+struct ScrollViewOffsetTracker: View {
+
+    var body: some View {
+        GeometryReader { geo in
+            Color.clear
+                .preference(
+                    key: ScrollOffsetPreferenceKey.self,
+                    value: geo.frame(in: .named(ScrollOffsetNamespace.namespace)).origin
+                )
+        }
+        .frame(height: 0)
+    }
+}
+
+private extension ScrollView {
+    func withOffsetTracking(
+        action: @escaping (_ offset: CGPoint) -> Void
+    ) -> some View {
+        self.coordinateSpace(name: ScrollOffsetNamespace.namespace)
+            .onPreferenceChange(ScrollOffsetPreferenceKey.self, perform: action)
+    }
+}
+
+public struct ScrollViewWithOffset<Content: View>: View {
+    
+    var offsetBinding : Binding<CGPoint>
+
+    public init(
+        _ axes: Axis.Set = .vertical,
+        showsIndicators: Bool = true,
+        offset: Binding<CGPoint>,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.axes = axes
+        self.showsIndicators = showsIndicators
+        self.offsetBinding = offset
+        self.content = content
+    }
+
+    private let axes: Axis.Set
+    private let showsIndicators: Bool
+    private let content: () -> Content
+
+    public typealias ScrollAction = (_ offset: CGPoint) -> Void
+
+    public var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            ZStack(alignment: .top) {
+                ScrollViewOffsetTracker()
+                content()
+            }
+        }.withOffsetTracking(action: onScroll)
+    }
+    
+    func onScroll(offset: CGPoint) {
+        self.offsetBinding.wrappedValue = offset
     }
 }
